662480fc033bf993d5d7d715731eed73f6e92a00
dosung_park <nsop4893@gmail.com>
2022-07-30 21:01:42 +0900

    ComponentDidMount 추가

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,15 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import Todo from './Todo';
-import React from 'react';
-import { Paper, List, Container } from "@material-ui/core"; 
-import AddTodo from './AddTodo';
+import logo from "./logo.svg";
+import "./App.css";
+import Todo from "./Todo";
+import React from "react";
+import { Paper, List, Container } from "@material-ui/core";
+import AddTodo from "./AddTodo";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
     };
+  }
+
+  componentDidMount() {
+    const requestOptions = {
+      method: "GET",
+      haders: { "Content-Type": "application/json" },
+    };
+
+    fetch("http://localhost:8080/todo", requestOptions)
+      .then((response) => {
+        response.json();
+      })
+      .then(
+        (response) => {
+          this.setState({
+            itmes: response.data,
+          });
+        },
+        (error) => {
+          this.setState({
+            error,
+          });
+        }
+      );
   }
 
   add = (item) => {
@@ -19,27 +43,27 @@ class App extends React.Component {
     thisItems.push(item);
     this.setState({ items: thisItems });
     console.log("item: " + this.state.items);
-  }
+  };
 
   delete = (item) => {
     const thisItems = this.state.items;
     console.log("Before update Items: ", this.state.items);
-    const newItems = thisItems.filter(e => e.id !== item.id);
+    const newItems = thisItems.filter((e) => e.id !== item.id);
     this.setState({ items: newItems }, () => {
       console.log("Update items: ", this.state.items);
     });
-  }
+  };
 
   render() {
     var todoItems = this.state.items.length > 0 && (
-      <Paper style={{margin: 16}}>
+      <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
             <Todo item={item} key={item.id} delete={this.delete} />
           ))}
         </List>
       </Paper>
-    )
+    );
     return (
       <div className="App">
         <Container maxWidth="md">
@@ -47,7 +71,7 @@ class App extends React.Component {
           <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
-      );
+    );
   }
 }
 
